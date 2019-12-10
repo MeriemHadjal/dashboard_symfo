ba5dc2a66d4491693b9017332a3e9409b2e153f3
MeriemHadjal <meriem.hadjalchateur@gmail.com>
2019-12-10 12:03:04 +0100

    redirection après connexion des users vers page enfants

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -94,8 +94,9 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('enfants_index'));
+        //ici on redirige l'utilisateur sur une autre page une fois logger
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl()
